85c6c9b46212ab44280ac8d9c00b460d85abef47
caiquefms <caiquef.ms@gmail.com>
2023-10-11 02:38:24 -0400

    [feat] - Constantes, extensões e estilos

---
+++ lib/app/core/design/text_style.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static final h1 = TextStyle(
+    fontSize: 32,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static final h2 = TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static final h3 = TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
+  );
+  static final h4 = TextStyle(
+    fontSize: 18,
+    color: Colors.white,
+    fontWeight: FontWeight.w700,
+    height: 1.3,
+  );
+  static final p1Regular = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0.25,
+  );
+  static final p1Semibold = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+  static final p1bold = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    height: 1.5,
+    letterSpacing: 0.25,
+  );
+  static final p2Regular = TextStyle(
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+    height: 1.71,
+  );
+  static final p2Semibold = TextStyle(
+    fontSize: 14,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    height: 1.71,
+  );
+  static final p3Regular = TextStyle(
+    fontSize: 12,
+    color: Colors.white,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+  static final p3Semibold = TextStyle(
+    fontSize: 12,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+}
---
+++ lib/app/core/infraestruture/config/config_constants.dart
@@ -0,0 +1,6 @@
+class ConfigConstants {
+  ConfigConstants._();
+
+  static String publicKey = "09880e6c5a6a9342cfaad83f18ad081e";
+  static String privatekey = "30fff849a766b1b49c043dfc26a4799e74cdf062";
+}
